c9ab22c40fb9776f8dee408b28feaba7a231e776
SvetlanaKanel <47015450+SvetlanaKanel@users.noreply.github.com>
2023-11-06 17:38:30 -0500

    TC_01.01.02 (#1)

--- cypress/pageObjects/HomePage.js
+++ cypress/pageObjects/HomePage.js
@@ -1,7 +1,13 @@
 class HomePage {
     //elements
+    getElements = () => cy.get('.category-cards >div:nth-child(1) h5');    
+    getForms = () => cy.get('.category-cards >div:nth-child(2) h5');
 
-    elements = () => cy.get('.category-cards >div:nth-child(1) h5');
+    // methods
+    clickElements(){
+        this.getElements().click();
+    }
+
 
 }
 export default HomePage
